6026f4471a277defa584c480c7488c41661d2fa7
myway <huliquns@126.com>
2017-02-25 22:15:11 +0800

    update

--- workserver/service/getCurrentResultSRV.py
+++ workserver/service/getCurrentResultSRV.py
@@ -21,7 +21,8 @@ class getCurrentResultResource(ServiceBase):
             self.errorReturn(GLBConfig.API_ERROR, 'userid 不存在.')
         
         matches = self.session.query(MatchData).\
-           filter(MatchData.userid == req_para['userid']).all()
+            filter(MatchData.status == GLBConfig.ENABLE).\
+            filter(MatchData.userid == req_para['userid']).all()
             
         maData = []
         if matches:
